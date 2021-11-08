9402fdd53ed8ef15d662499bb7ecc903f803155c
RomanSchaposhnikov <roman.schaposhnikov@gmail.com>
2021-11-08 11:06:14 +0300

    A little bit refactored dict_api.py

--- bot/dict_api.py
+++ bot/dict_api.py
@@ -4,65 +4,60 @@ import requests
 from loguru import logger as log
 
 
+def get_phrase_definition(phrase: str, url: str) -> requests.models.Response:
+    return requests.get(url + phrase)
+
+
 def get_dict_definition(phrase: str) -> str:
 
-    api_url = "https://api.dictionaryapi.dev/api/v2/entries/en/"
-    response = requests.get(api_url + phrase)
+    response = get_phrase_definition(
+        phrase=phrase,
+        url="https://api.dictionaryapi.dev/api/v2/entries/en/"
+    )
     
-    if response:
-        data = json.loads(response.text)[0]
+    if not response:
+        return "Sorry, no result found"
 
-        result = []
+    data = json.loads(response.text)[0]
+    result = []
+    # log.debug(data)
 
-        log.debug(data)
 
+    try:
+        result.append(f"<b>Phonetic</b>: {data['phonetic']}\n")
+    except KeyError as key_err:
+        log.info(f"Didn't find key {key_err}")
 
+        
+    for meaning in data["meanings"]:
         try:
-            result.append(f"<b>Phonetic</b>: {data['phonetic']}\n")
-
+            result.append(f"""\n\
+<b>Part of speech</b>: \
+<i>{meaning['partOfSpeech']}</i>"""
+        )
         except KeyError as key_err:
             log.info(f"Didn't find key {key_err}")
 
-            
-        meanings = data["meanings"]
-
-        for meaning in meanings:
-
+        
+        for definition in meaning["definitions"]:
+            #log.debug(definition) 
             try:
-                result.append(f"""\n\
-<b>Part of speech</b>: \
-<i>{meaning['partOfSpeech']}</i>"""
-            )
-            except KeyError as key_err:
-                log.info(f"Didn't find key {key_err}")
+                for k, v in definition.items():
 
+                    match k, v:
+                        case "definition", _:
+                            result.append(f"\n<b>{k}</b>: <i>{v}</i>\n") 
+                        case _, []:
+                            pass
+                        case _, "":
+                            pass
+                        case _:
+                            result.append(f"<b>{k}</b>: <i>{v}</i>\n") 
+
+            except ValueError as val_err:
+                log.error(val_err)
             
-            for definition in meaning["definitions"]:
-
-                #log.debug(definition) 
-                try:
-                    for k, v in definition.items():
-
-                        match k, v:
-                            case "definition", _:
-                                result.append(f"\n<b>{k}</b>: <i>{v}</i>\n") 
-                            case _, []:
-                                pass
-                            case _, "":
-                                pass
-                            case _:
-                                result.append(f"<b>{k}</b>: <i>{v}</i>\n") 
-
-
-                except ValueError as val_err:
-                    log.error(val_err)
-
-                
-        return "\n".join(result)
-
-
-    else:
-        return "Sorry, no result found"
+    return "\n".join(result)
 
 
 if __name__ == "__main__":
